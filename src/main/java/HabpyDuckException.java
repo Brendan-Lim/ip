30b6ed58873799c8af2c9baf4f140f7ffed9bd58
Brendan Lim <121050815+Brendan-Lim@users.noreply.github.com>
2026-08-17 14:41:06 +0800

    Add bot exception file

---
+++ src/main/java/HabpyDuckException.java
@@ -0,0 +1,13 @@
+/**
+ * Represents an error caused by invalid user input.
+ */
+public class HabpyDuckException extends Exception {
+    /**
+     * Creates an exception with a message that can be shown to the user.
+     *
+     * @param message the explanation of what went wrong
+     */
+    public HabpyDuckException(String message) {
+        super(message);
+    }
+}
