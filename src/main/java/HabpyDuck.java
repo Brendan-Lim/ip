243f24407e9481a0615a9976816199b0c4c9b05c
Brendan Lim <121050815+Brendan-Lim@users.noreply.github.com>
2026-08-17 13:30:04 +0800

    Changed code to greet and exit

--- src/main/java/HabpyDuck.java
+++ src/main/java/HabpyDuck.java
@@ -1,4 +1,7 @@
 public class HabpyDuck {
+    private static final String CHATBOT_NAME = "HabpyDuck";
+    private static final String SEPARATOR = "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner = " _   _       _                 ____             _    \n"
                 + "| | | | __ _| |__  _ __  _   _|  _ \\ _   _  ___| | __\n"
@@ -6,6 +9,12 @@ public class HabpyDuck {
                 + "|  _  | (_| | |_) | |_) | |_| | |_| | |_| | (__|   < \n"
                 + "|_| |_|\\__,_|_.__/| .__/ \\__, |____/ \\__,_|\\___|_|\\_\\\n"
                 + "                  |_|    |___/                       \n";
-        System.out.println(banner);
+        System.out.println(SEPARATOR);
+        System.out.print(banner);
+        System.out.println("Hi friend! I'm " + CHATBOT_NAME + ".");
+        System.out.println("What can I do for you today?");
+        System.out.println(SEPARATOR);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(SEPARATOR);
     }
 }
