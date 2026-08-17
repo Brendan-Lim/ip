34a3e22e1a40774794da830c180d865f7980194b
Brendan Lim <121050815+Brendan-Lim@users.noreply.github.com>
2026-08-17 14:11:23 +0800

    Add support for ToDos, Events, Deadlines with inheritance

--- src/main/java/HabpyDuck.java
+++ src/main/java/HabpyDuck.java
@@ -30,23 +30,51 @@ public class HabpyDuck {
             if (command.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] "
-                            + tasks[i].getDescription());
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
             } else if (command.startsWith("mark ")) {
                 int taskNumber = Integer.parseInt(command.substring(5));
                 int taskIndex = taskNumber - 1;
                 tasks[taskIndex].markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  [X] " + tasks[taskIndex].getDescription());
+                System.out.println("  " + tasks[taskIndex]);
             } else if (command.startsWith("unmark ")) {
                 int taskNumber = Integer.parseInt(command.substring(7));
                 int taskIndex = taskNumber - 1;
                 tasks[taskIndex].markAsNotDone();
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  [ ] " + tasks[taskIndex].getDescription());
+                System.out.println("  " + tasks[taskIndex]);
+            } else if (command.startsWith("todo ")) {
+                String description = command.substring(5);
+                tasks[taskCount] = new Todo(description);
+                taskCount++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+            } else if (command.startsWith("deadline ")) {
+                String taskDetails = command.substring(9);
+                String[] parts = taskDetails.split(" /by ", 2);
+                String description = parts[0];
+                String by = parts[1];
+                tasks[taskCount] = new Deadline(description, by);
+                taskCount++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+            } else if (command.startsWith("event ")) {
+                String taskDetails = command.substring(6);
+                String[] descriptionAndTimes = taskDetails.split(" /from ", 2);
+                String description = descriptionAndTimes[0];
+                String[] times = descriptionAndTimes[1].split(" /to ", 2);
+                String from = times[0];
+                String to = times[1];
+                tasks[taskCount] = new Event(description, from, to);
+                taskCount++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
             } else {
-                tasks[taskCount] = new Task(command);
+                tasks[taskCount] = new Todo(command);
                 taskCount++;
                 System.out.println("added: " + command);
             }
